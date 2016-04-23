09096ee4784309f5424a7adeb93c402585b9b309
dan <dan@DAN-ULTRA>
2016-04-23 16:27:23 -0700

    This file was missing.

---
+++ MS.SyncFrame/NamespaceDoc.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="NamespaceDoc.cs" company="MS">
+//     Copyright (c) 2016 MS
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MS.SyncFrame
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// The <see cref="MS.SyncFrame"/> namespace contains classes, enumerations and structures for the SyncFrame API.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
